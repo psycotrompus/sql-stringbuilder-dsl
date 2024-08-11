9a52374ed7f6f48788e88f49f8aa97976e6a4db8
EJ <ejlayco@gmail.com>
2024-08-12 07:46:53 +0800

    Issues ready for version 0.0.11 release:
    * Issue #11 - Implement other comparators

--- src/main/java/io/github/psycotrompus/sql/SqlColumn.java
+++ src/main/java/io/github/psycotrompus/sql/SqlColumn.java
@@ -3,7 +3,8 @@ package io.github.psycotrompus.sql;
 import static io.github.psycotrompus.sql.SqlUtils.isBlank;
 
 /**
- * Represents a column definition when generating SQL queries.
+ * Represents a column definition when generating SQL queries. To generate a column, use the method
+ * {@link SqlTable#column(String)}.
  *
  * @author ejlayco
  */
@@ -21,7 +22,7 @@ public class SqlColumn extends PartialSql {
 	 * @param table a {@link io.github.psycotrompus.sql.SqlTable} object
 	 * @param name a {@link java.lang.String} object
 	 */
-	public SqlColumn(SqlTable table, String name) {
+	SqlColumn(SqlTable table, String name) {
 		this(table, name, null);
 	}
 
@@ -32,92 +33,154 @@ public class SqlColumn extends PartialSql {
 	 * @param name a {@link java.lang.String} object
 	 * @param alias a {@link java.lang.String} object
 	 */
-	public SqlColumn(SqlTable table, String name, String alias) {
+	SqlColumn(SqlTable table, String name, String alias) {
 		this.table = table;
 		this.name = name;
 		this.alias = alias;
 	}
 
 	/**
-	 * <p>eq.</p>
+	 * SQL predicate/filter that checks if this column is equal to the parameter value.
 	 *
-	 * @param parameter a {@link java.lang.String} object
-	 * @return a {@link io.github.psycotrompus.sql.SqlTypeFilter} object
+	 * @param parameter Name of this parameter.
+	 * @return A {@link SqlTypeFilter} object that represents the partial SQL statement.
 	 */
 	public SqlTypeFilter eq(String parameter) {
 		return new SqlValueFilter(this, "=", ":" + parameter);
 	}
 
 	/**
-	 * <p>in.</p>
+	 * Creates the predicate/filter if this column's value is present in this parameter.
 	 *
-	 * @param parameter a {@link java.lang.String} object
-	 * @return a {@link io.github.psycotrompus.sql.SqlTypeFilter} object
+	 * @param parameter Name of this parameter.
+	 * @return A {@link SqlTypeFilter} object that represents the partial SQL statement.
 	 */
 	public SqlTypeFilter in(String parameter) {
 		return new SqlValueFilter(this, "IN", ":" + parameter);
 	}
 
 	/**
-	 * <p>isTrue.</p>
+	 * Adds a filter if this column's value is <code>true</code>.
 	 *
-	 * @return a {@link io.github.psycotrompus.sql.SqlTypeFilter} object
+	 * @return a {@link SqlTypeFilter} object that represents the partial SQL statement.
 	 */
 	public SqlTypeFilter isTrue() {
 		return new SqlValueFilter(this, "IS", "true");
 	}
 
 	/**
-	 * <p>isFalse.</p>
+	 * Adds a filter if this column's value is <code>false</code>.
 	 *
-	 * @return a {@link io.github.psycotrompus.sql.SqlTypeFilter} object
+	 * @return a {@link SqlTypeFilter} object that represents the partial SQL statement.
 	 */
 	public SqlTypeFilter isFalse() {
 		return new SqlValueFilter(this, "IS", "false");
 	}
 
 	/**
-	 * <p>isNull.</p>
+	 * Adds a filter if this column's value is <code>NULL</code>.
 	 *
-	 * @return a {@link io.github.psycotrompus.sql.SqlTypeFilter} object
+	 * @return a {@link SqlTypeFilter} object that represents the partial SQL statement.
 	 */
 	public SqlTypeFilter isNull() {
 		return new SqlValueFilter(this, "IS", "null");
 	}
 
 	/**
-	 * <p>like.</p>
+	 * Adds a filter if this column's value matches the parameter value using the
+	 * <code>LIKE</code> operator.
 	 *
-	 * @param parameter a {@link java.lang.String} object
-	 * @return a {@link io.github.psycotrompus.sql.SqlTypeFilter} object
+	 * @param parameter Name of this parameter.
+	 * @return A {@link SqlTypeFilter} object that represents the partial SQL statement.
 	 */
 	public SqlTypeFilter like(String parameter) {
 		return new SqlValueFilter(this, "LIKE", ":" + parameter);
 	}
 
 	/**
-	 * <p>eq.</p>
+	 * Adds a filter if this column's value is <strong>greater than</strong> the parameter value.
 	 *
-	 * @param column a {@link io.github.psycotrompus.sql.SqlColumn} object
-	 * @return a {@link io.github.psycotrompus.sql.SqlTypeFilter} object
+	 * @param parameter Name of this parameter.
+	 * @return A {@link SqlTypeFilter} object that represents the partial SQL statement.
+	 */
+	public SqlTypeFilter gt(String parameter) {
+		return new SqlValueFilter(this, ">", ":" + parameter);
+	}
+
+	/**
+	 * Adds a filter if this column's value is <b>less than</b> the parameter value.
+	 *
+	 * @param parameter Name of this parameter.
+	 * @return A {@link SqlTypeFilter} object that represents the partial SQL statement.
+	 */
+	public SqlTypeFilter lt(String parameter) {
+		return new SqlValueFilter(this, "<", ":" + parameter);
+	}
+
+	/**
+	 * Adds a filter if this column's value is <strong>greater than or equal to</strong>
+	 * the parameter value.
+	 *
+	 * @param parameter Name of this parameter.
+	 * @return A {@link SqlTypeFilter} object that represents the partial SQL statement.
+	 */
+	public SqlTypeFilter gte(String parameter) {
+		return new SqlValueFilter(this, ">=", ":" + parameter);
+	}
+
+	/**
+	 * Adds a filter if this column's value is <b>less than or equal to</b> the parameter value.
+	 *
+	 * @param parameter Name of this parameter.
+	 * @return A {@link SqlTypeFilter} object that represents the partial SQL statement.
+	 */
+	public SqlTypeFilter lte(String parameter) {
+		return new SqlValueFilter(this, "<=", ":" + parameter);
+	}
+
+	/**
+	 * Adds a filter if this column's value is <b>NOT equal to</b> the parameter value.
+	 *
+	 * @param parameter Name of this parameter.
+	 * @return A {@link SqlTypeFilter} object that represents the partial SQL statement.
+	 */
+	public SqlTypeFilter ne(String parameter) {
+		return new SqlValueFilter(this, "<>", ":" + parameter);
+	}
+
+	/**
+	 * Adds a filter if this column's value is equal to another column value.
+	 *
+	 * @param column The {@link SqlColumn} object that will equate to this column
+	 * @return a {@link SqlTypeFilter} object that represents the partial SQL statement.
 	 */
 	public SqlTypeFilter eq(SqlColumn column) {
 		return new SqlColumnFilter(this, "=", column);
 	}
 
 	/**
-	 * <p>asc.</p>
+	 * Adds a filter if this column's value is NOT equal to another column value.
 	 *
-	 * @return a {@link io.github.psycotrompus.sql.SqlOrder} object
+	 * @param column The {@link SqlColumn} object that will equate to this column
+	 * @return a {@link SqlTypeFilter} object that represents the partial SQL statement.
+	 */
+	public SqlTypeFilter ne(SqlColumn column) {
+		return new SqlColumnFilter(this, "<>", column);
+	}
+
+	/**
+	 * Creates a partial SQL statement that returns the result in ascending order using this column
+	 *
+	 * @return A {@link SqlOrder} object that represents the partial SQL statement.
 	 */
 	public SqlOrder asc() {
 		return new SqlOrder(this, false);
 	}
 
 	/**
-	 * <p>desc.</p>
+	 * Creates a partial SQL statement that returns the result in descending order using this column
 	 *
-	 * @return a {@link io.github.psycotrompus.sql.SqlOrder} object
+	 * @return A {@link SqlOrder} object that represents the partial SQL statement.
 	 */
 	public SqlOrder desc() {
 		return new SqlOrder(this, true);
